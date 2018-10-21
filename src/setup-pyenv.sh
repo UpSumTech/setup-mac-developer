6d7d3358d8a26668a66c00dafefca148548caa71
Suman Mukherjee <sumanmukherjee03@gmail.com>
2018-10-21 10:05:52 -0700

    Install python 3.6.6 with pyenv

--- src/setup-pyenv.sh
+++ src/setup-pyenv.sh
@@ -4,6 +4,7 @@ install_pythons() {
   local python_version
   local arr=('2.7.14' \
     '3.4.4' \
+    '3.6.6' \
   )
   for python_version in "${arr[@]}"; do
     . "$HOME/.bash_profile" && env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -fk "$python_version"
@@ -18,6 +19,7 @@ main() {
   cd $HOME/lib
   ln -s $HOME/.pyenv/versions/2.7.14/lib/libpython2.7.dylib
   ln -s $HOME/.pyenv/versions/3.4.4/lib/libpython3.4m.dylib
+  ln -s $HOME/.pyenv/versions/3.6.6/lib/libpython3.6m.dylib
   popd
 }
 
