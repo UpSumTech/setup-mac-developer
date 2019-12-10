8e7657061ca3d60e892a7f85b8a69702b009d326
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-12-10 11:19:52 -0800

    Updated the script for python installation

--- src/setup-pyenv.sh
+++ src/setup-pyenv.sh
@@ -1,24 +1,43 @@
 #!/usr/bin/env bash
 
-install_pythons() {
-  local python_version
+install_python() {
+  local python_version="$1"
+
+  PYENV_CFLAGS="-I$(brew --prefix openssl)/include"
+  PYENV_CFLAGS="-I$(brew --prefix zlib)/include $PYENV_CFLAGS"
+  PYENV_CFLAGS="-I$(brew --prefix readline)/include $PYENV_CFLAGS"
+  PYENV_CFLAGS="-I$(brew --prefix sqlite)/include $PYENV_CFLAGS"
+
+  PYENV_LDFLAGS="-L$(brew --prefix openssl)/lib"
+  PYENV_LDFLAGS="-L$(brew --prefix zlib)/lib $PYENV_LDFLAGS"
+  PYENV_LDFLAGS="-L$(brew --prefix readline)/lib $PYENV_LDFLAGS"
+  PYENV_LDFLAGS="-L$(brew --prefix sqlite)/lib $PYENV_LDFLAGS"
+
+  PYENV_PYTHON_CONFIGURE_OPTS="--enable-shared"
+  env CFLAGS="$PYENV_CFLAGS" LDFLAGS="$PYENV_LDFLAGS" PYTHON_CONFIGURE_OPTS="$PYENV_PYTHON_CONFIGURE_OPTS" pyenv install -fk "$python_version"
+}                                                                            
+                                                                             
+install_pythons() {                                                          
   local arr=('2.7.14' \
-    '3.4.4' \
+    '3.5.4' \
     '3.6.6' \
   )
+
+  local python_version
+  . "$HOME/.bash_profile"
   for python_version in "${arr[@]}"; do
-    . "$HOME/.bash_profile" && env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -fk "$python_version"
+    install_python $python_version
   done
 }
 
 main() {
   install_pythons
-  . $HOME/.bash_profile && pyenv global 2.7.14 3.4.4
+  . $HOME/.bash_profile && pyenv global 2.7.14 3.5.4
   pushd .
   mkdir -p $HOME/lib
   cd $HOME/lib
   ln -s $HOME/.pyenv/versions/2.7.14/lib/libpython2.7.dylib
-  ln -s $HOME/.pyenv/versions/3.4.4/lib/libpython3.4m.dylib
+  ln -s $HOME/.pyenv/versions/3.5.4/lib/libpython3.5m.dylib
   ln -s $HOME/.pyenv/versions/3.6.6/lib/libpython3.6m.dylib
   popd
 }
