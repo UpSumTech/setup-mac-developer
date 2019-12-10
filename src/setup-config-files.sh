e5357f5f7db122fe07bbeb4c689ff302ea5748d2
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-12-10 15:27:50 -0800

    Updated the conf files copy script to copy over cheat config

--- src/setup-config-files.sh
+++ src/setup-config-files.sh
@@ -8,6 +8,7 @@ main() {
   cp "$ROOT_DIR/templates/inputrc" "$HOME/.inputrc"
   cp "$ROOT_DIR/templates/ctags" "$HOME/.ctags"
   cp "$ROOT_DIR/templates/tigrc" "$HOME/.tigrc"
+  mkdir -p "$HOME/.config/cheat && cp "$ROOT_DIR/templates/cheatconf.yml" "$HOME/.config/cheat/conf.yml"
 }
 
 [[ "$BASH_SOURCE" == "$0" ]] && main "$@"
