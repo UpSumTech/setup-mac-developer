b43089cfe7fc7ce950ead8cea646202c9a2e732e
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-12-10 11:43:31 -0800

    Updated the script for ruby installation

--- src/setup-rbenv.sh
+++ src/setup-rbenv.sh
@@ -3,20 +3,16 @@
 install_rubies() {
   local ruby_version
   local arr=('2.4.0' \
+    '2.5.0' \
   )
   for ruby_version in "${arr[@]}"; do
-    CONFIGURE_OPTS='--enable-shared' rbenv install "$ruby_version"
+    rbenv install "$ruby_version"
   done
 }
-
-# fix_openssl() {
-  # brew link --force openssl
-# }
 
 main() {
   install_rubies
   . $HOME/.bash_profile && rbenv global 2.4.0 && rbenv rehash
-  # fix_openssl
 }
 
 [[ "$BASH_SOURCE" == "$0" ]] && main "$@"
