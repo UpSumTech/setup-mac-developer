88f1fa03ac49d0c87dc58785987f4d719ccc140e
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-12-11 17:12:36 -0800

    Activating profiles for aws setup

--- templates/activate_profile.sh
+++ templates/activate_profile.sh
@@ -3,8 +3,6 @@
 all_available_profiles() {
   local profiles=( \
     "admin" \
-    "superuser" \
-    "developer" \
   )
   echo "${profiles[@]}"
 }
@@ -19,17 +17,10 @@ which_profile_is_active() {
 
 clean_active_profile() {
   case $CURRENT_PROFILE in
-  developer)
-    deactivate_profile_for_developer
-    echo "Unset current profile from developer"
-    ;;
   admin)
-    deactivate_profile_for_admin
+    deactivate_profile_for_personal_admin
+    deactivate_profile_for_work_admin
     echo "Unset current profile from admin"
-    ;;
-  superuser)
-    deactivate_profile_for_superuser
-    echo "Unset current profile from superuser"
     ;;
   *)
     echo "No valid profile currently active"
@@ -37,69 +28,49 @@ clean_active_profile() {
   esac
 }
 
-activate_profile_for_developer() {
+activate_profile_for_personal_admin() {
   clean_active_profile
-  export AWS_ACCESS_KEY_ID="$DEVELOPER_AWS_ACCESS_KEY_ID"
-  export AWS_SECRET_ACCESS_KEY="$DEVELOPER_AWS_SECRET_ACCESS_KEY"
-  export AWS_ACCOUNT_ID="$DEVELOPER_AWS_ACCOUNT_ID"
-  export AWS_ACCESS_KEY="$DEVELOPER_AWS_ACCESS_KEY_ID"
-  export AWS_SECRET_KEY="$DEVELOPER_AWS_SECRET_ACCESS_KEY"
-  export AWS_DEFAULT_REGION="$DEVELOPER_AWS_DEFAULT_REGION"
-  export DEPLOY_GITHUB_TOKEN="$DEVELOPER_DEPLOY_GITHUB_TOKEN"
-  export DOCKERHUB_USERNAME="$DEVELOPER_DOCKERHUB_USERNAME"
-  export DOCKERHUB_PASSWORD="$DEVELOPER_DOCKERHUB_PASSWORD"
-  export GITHUB_USERNAME="$DEVELOPER_GITHUB_USERNAME"
-  export BINTRAY_USERNAME="$DEVELOPER_BINTRAY_USERNAME"
-  export BINTRAY_API_KEY="$DEVELOPER_BINTRAY_API_KEY"
-  export BINTRAY_REPO_NAME="$DEVELOPER_BINTRAY_REPO_NAME"
-  activate_profile_helper "developer"
-}
-
-deactivate_profile_for_developer() {
-  deactivate_profile_helper
-}
-
-activate_profile_for_admin() {
-  clean_active_profile
-  export AWS_ACCESS_KEY_ID="$ADMIN_AWS_ACCESS_KEY_ID"
-  export AWS_SECRET_ACCESS_KEY="$ADMIN_AWS_SECRET_ACCESS_KEY"
-  export AWS_ACCOUNT_ID="$ADMIN_AWS_ACCOUNT_ID"
-  export AWS_ACCESS_KEY="$ADMIN_AWS_ACCESS_KEY_ID"
-  export AWS_SECRET_KEY="$ADMIN_AWS_SECRET_ACCESS_KEY"
-  export AWS_DEFAULT_REGION="$ADMIN_AWS_DEFAULT_REGION"
-  export DEPLOY_GITHUB_TOKEN="$ADMIN_DEPLOY_GITHUB_TOKEN"
-  export DOCKERHUB_USERNAME="$ADMIN_DOCKERHUB_USERNAME"
-  export DOCKERHUB_PASSWORD="$ADMIN_DOCKERHUB_PASSWORD"
-  export GITHUB_USERNAME="$ADMIN_GITHUB_USERNAME"
-  export BINTRAY_USERNAME="$ADMIN_BINTRAY_USERNAME"
-  export BINTRAY_API_KEY="$ADMIN_BINTRAY_API_KEY"
-  export BINTRAY_REPO_NAME="$ADMIN_BINTRAY_REPO_NAME"
+  export AWS_ACCESS_KEY_ID="$PERSONAL_ADMIN_AWS_ACCESS_KEY_ID"
+  export AWS_SECRET_ACCESS_KEY="$PERSONAL_ADMIN_AWS_SECRET_ACCESS_KEY"
+  export AWS_ACCOUNT_ID="$PERSONAL_ADMIN_AWS_ACCOUNT_ID"
+  export AWS_ACCESS_KEY="$PERSONAL_ADMIN_AWS_ACCESS_KEY_ID"
+  export AWS_SECRET_KEY="$PERSONAL_ADMIN_AWS_SECRET_ACCESS_KEY"
+  export AWS_DEFAULT_REGION="$PERSONAL_ADMIN_AWS_DEFAULT_REGION"
+  export DEPLOY_GITHUB_TOKEN="$PERSONAL_ADMIN_DEPLOY_GITHUB_TOKEN"
+  export DOCKERHUB_USERNAME="$PERSONAL_ADMIN_DOCKERHUB_USERNAME"
+  export DOCKERHUB_PASSWORD="$PERSONAL_ADMIN_DOCKERHUB_PASSWORD"
+  export GITHUB_USERNAME="$PERSONAL_ADMIN_GITHUB_USERNAME"
+  export BINTRAY_USERNAME="$PERSONAL_ADMIN_BINTRAY_USERNAME"
+  export BINTRAY_API_KEY="$PERSONAL_ADMIN_BINTRAY_API_KEY"
+  export BINTRAY_REPO_NAME="$PERSONAL_ADMIN_BINTRAY_REPO_NAME"
+  export AWS_PROFILE="personal_root"
   activate_profile_helper "admin"
 }
 
-deactivate_profile_for_admin() {
+deactivate_profile_for_personal_admin() {
   deactivate_profile_helper
 }
 
-activate_profile_for_superuser() {
+activate_profile_for_work_admin() {
   clean_active_profile
-  export AWS_ACCESS_KEY_ID="$SUPERUSER_AWS_ACCESS_KEY_ID"
-  export AWS_SECRET_ACCESS_KEY="$SUPERUSER_AWS_SECRET_ACCESS_KEY"
-  export AWS_ACCOUNT_ID="$SUPERUSER_AWS_ACCOUNT_ID"
-  export AWS_ACCESS_KEY="$SUPERUSER_AWS_ACCESS_KEY_ID"
-  export AWS_SECRET_KEY="$SUPERUSER_AWS_SECRET_ACCESS_KEY"
-  export AWS_DEFAULT_REGION="$SUPERUSER_AWS_DEFAULT_REGION"
-  export DEPLOY_GITHUB_TOKEN="$SUPERUSER_DEPLOY_GITHUB_TOKEN"
-  export DOCKERHUB_USERNAME="$SUPERUSER_DOCKERHUB_USERNAME"
-  export DOCKERHUB_PASSWORD="$SUPERUSER_DOCKERHUB_PASSWORD"
-  export GITHUB_USERNAME="$SUPERUSER_GITHUB_USERNAME"
-  export BINTRAY_USERNAME="$SUPERUSER_BINTRAY_USERNAME"
-  export BINTRAY_API_KEY="$SUPERUSER_BINTRAY_API_KEY"
-  export BINTRAY_REPO_NAME="$SUPERUSER_BINTRAY_REPO_NAME"
-  activate_profile_helper "superuser"
+  export AWS_ACCESS_KEY_ID="$WORK_ADMIN_AWS_ACCESS_KEY_ID"
+  export AWS_SECRET_ACCESS_KEY="$WORK_ADMIN_AWS_SECRET_ACCESS_KEY"
+  export AWS_ACCOUNT_ID="$WORK_ADMIN_AWS_ACCOUNT_ID"
+  export AWS_ACCESS_KEY="$WORK_ADMIN_AWS_ACCESS_KEY_ID"
+  export AWS_SECRET_KEY="$WORK_ADMIN_AWS_SECRET_ACCESS_KEY"
+  export AWS_DEFAULT_REGION="$WORK_ADMIN_AWS_DEFAULT_REGION"
+  export DEPLOY_GITHUB_TOKEN="$WORK_ADMIN_DEPLOY_GITHUB_TOKEN"
+  export DOCKERHUB_USERNAME="$WORK_ADMIN_DOCKERHUB_USERNAME"
+  export DOCKERHUB_PASSWORD="$WORK_ADMIN_DOCKERHUB_PASSWORD"
+  export GITHUB_USERNAME="$WORK_ADMIN_GITHUB_USERNAME"
+  export BINTRAY_USERNAME="$WORK_ADMIN_BINTRAY_USERNAME"
+  export BINTRAY_API_KEY="$WORK_ADMIN_BINTRAY_API_KEY"
+  export BINTRAY_REPO_NAME="$WORK_ADMIN_BINTRAY_REPO_NAME"
+  export AWS_PROFILE="work_root"
+  activate_profile_helper "admin"
 }
 
-deactivate_profile_for_superuser() {
+deactivate_profile_for_work_admin() {
   deactivate_profile_helper
 }
 
@@ -123,22 +94,24 @@ deactivate_profile_helper() {
   unset BINTRAY_USERNAME
   unset BINTRAY_API_KEY
   unset BINTRAY_REPO_NAME
+  unset AWS_ROLE_ARN
+  unset AWS_MFA_SERIAL
+  unset AWS_PROFILE
 }
 
 activate_help() {
   echo "
     Valid profiles are : $(_all_available_profiles)
     Valid options for calling acivate profile are
-      activate_profile_for_developer
-      deactivate_profile_for_developer
-      activate_profile_for_admin
-      deactivate_profile_for_admin
-      activate_profile_for_superuser
-      deactivate_profile_for_superuser
+      activate_profile_for_personal_admin
+      deactivate_profile_for_personal_admin
+      activate_profile_for_work_admin
+      deactivate_profile_for_work_admin
       activate_help
       all_available_profiles
       clean_active_profile
   "
 }
 
+[[ -f "$HOME/.work_helpers.sh" && -z $SOURCED_WORK_HELPERS ]] && . "$HOME/.work_helpers.sh"
 export SOURCED_ACTIVATE_PROFILE=1
