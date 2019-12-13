4545bbfcffcf0d30501a6cd78c9966f9d0ecff02
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-12-13 10:57:40 -0800

    Updated the helper scripts to be able to use profiles and assume roles

--- templates/activate_profile.sh
+++ templates/activate_profile.sh
@@ -15,17 +15,34 @@ which_profile_is_active() {
   fi
 }
 
+setup_aws_session_token() {
+  local creds_file
+  local aws_access_key_id
+  local aws_secret_access_key
+  local aws_session_token
+  if [[ ! -z "$AWS_ROLE_ARN" ]]; then
+    if [[ ! -z "$AWS_MFA_ARN" ]]; then
+      rm -rf "$HOME/.aws/cli/cache"
+      # aws sts get-session-token --profile $AWS_PROFILE --duration-seconds 43200
+      aws sts get-caller-identity --profile $AWS_PROFILE
+      creds_file="$(find ~/.aws/cli/cache/ -type f)"
+      aws_access_key_id="$(cat $creds_file | jq -r '.Credentials.AccessKeyId')"
+      aws_secret_access_key="$(cat $creds_file | jq -r '.Credentials.SecretAccessKey')"
+      aws_session_token="$(cat $creds_file | jq -r '.Credentials.SessionToken')"
+      export AWS_ACCESS_KEY_ID="$aws_access_key_id"
+      export AWS_SECRET_ACCESS_KEY="$aws_secret_access_key"
+      export AWS_ACCESS_KEY="$aws_access_key_id"
+      export AWS_SECRET_KEY="$aws_secret_access_key"
+      export AWS_SESSION_TOKEN="$aws_session_token"
+    fi
+  fi
+}
+
 clean_active_profile() {
-  case $CURRENT_PROFILE in
-  admin)
-    deactivate_profile_for_personal_admin
-    deactivate_profile_for_work_admin
-    echo "Unset current profile from admin"
-    ;;
-  *)
-    echo "No valid profile currently active"
-    ;;
-  esac
+  rm -rf "$HOME/.aws/cli/cache"
+  deactivate_profile_for_personal_admin
+  deactivate_profile_for_work_admin
+  echo "Unset current profile from $CURRENT_PROFILE"
 }
 
 activate_profile_for_personal_admin() {
@@ -36,6 +53,7 @@ activate_profile_for_personal_admin() {
   export AWS_ACCESS_KEY="$PERSONAL_ADMIN_AWS_ACCESS_KEY_ID"
   export AWS_SECRET_KEY="$PERSONAL_ADMIN_AWS_SECRET_ACCESS_KEY"
   export AWS_DEFAULT_REGION="$PERSONAL_ADMIN_AWS_DEFAULT_REGION"
+  export AWS_DEFAULT_OUTPUT="json"
   export DEPLOY_GITHUB_TOKEN="$PERSONAL_ADMIN_DEPLOY_GITHUB_TOKEN"
   export DOCKERHUB_USERNAME="$PERSONAL_ADMIN_DOCKERHUB_USERNAME"
   export DOCKERHUB_PASSWORD="$PERSONAL_ADMIN_DOCKERHUB_PASSWORD"
@@ -59,6 +77,7 @@ activate_profile_for_work_admin() {
   export AWS_ACCESS_KEY="$WORK_ADMIN_AWS_ACCESS_KEY_ID"
   export AWS_SECRET_KEY="$WORK_ADMIN_AWS_SECRET_ACCESS_KEY"
   export AWS_DEFAULT_REGION="$WORK_ADMIN_AWS_DEFAULT_REGION"
+  export AWS_DEFAULT_OUTPUT="json"
   export DEPLOY_GITHUB_TOKEN="$WORK_ADMIN_DEPLOY_GITHUB_TOKEN"
   export DOCKERHUB_USERNAME="$WORK_ADMIN_DOCKERHUB_USERNAME"
   export DOCKERHUB_PASSWORD="$WORK_ADMIN_DOCKERHUB_PASSWORD"
@@ -85,8 +104,10 @@ deactivate_profile_helper() {
   unset AWS_SECRET_ACCESS_KEY
   unset AWS_ACCESS_KEY
   unset AWS_SECRET_KEY
+  unset AWS_SESSION_TOKEN
   unset AWS_ACCOUNT_ID
   unset AWS_DEFAULT_REGION
+  unset AWS_DEFAULT_OUTPUT
   unset DEPLOY_GITHUB_TOKEN
   unset DOCKERHUB_USERNAME
   unset DOCKERHUB_PASSWORD
@@ -95,7 +116,7 @@ deactivate_profile_helper() {
   unset BINTRAY_API_KEY
   unset BINTRAY_REPO_NAME
   unset AWS_ROLE_ARN
-  unset AWS_MFA_SERIAL
+  unset AWS_MFA_ARN
   unset AWS_PROFILE
 }
 
@@ -105,8 +126,6 @@ activate_help() {
     Valid options for calling acivate profile are
       activate_profile_for_personal_admin
       deactivate_profile_for_personal_admin
-      activate_profile_for_work_admin
-      deactivate_profile_for_work_admin
       activate_help
       all_available_profiles
       clean_active_profile
