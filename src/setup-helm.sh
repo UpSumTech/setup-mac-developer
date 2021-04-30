5b48dcd87984b09a81cd44dadddd13346c9ffb8f
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-04-30 10:36:31 -0700

    Remove one helm plugin which isnt being used

--- src/setup-helm.sh
+++ src/setup-helm.sh
@@ -3,7 +3,6 @@
 enable_helm_plugins() {
   helm plugin install https://github.com/databus23/helm-diff
   helm plugin install https://github.com/adamreese/helm-nuke
-  helm plugin install https://github.com/adamreese/helm-env
   helm plugin install https://github.com/adamreese/helm-last
   helm plugin install https://github.com/aslafy-z/helm-git --version 0.10.0
   helm plugin install https://github.com/instrumenta/helm-kubeval
