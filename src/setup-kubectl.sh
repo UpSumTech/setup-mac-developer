2c1f928bbae429435aee2dc36d231803550741c9
Suman Mukherjee <sumanmukherjee@bitgo.com>
2022-06-22 21:27:02 -0700

    Update the kubectl install scripts to also include kubectl-convert

--- src/setup-kubectl.sh
+++ src/setup-kubectl.sh
@@ -20,10 +20,22 @@ install_plugins_with_krew() {
     who-can
 }
 
+install_kube_convert() {
+  (
+    set -x; cd "$(mktemp -d)" &&
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/$OS/$ARCH/kubectl-convert" &&
+    chmod +x kubectl-convert &&
+    mv kubectl-convert $HOME/bin/kubectl-convert
+  )
+}
+
 main() {
   brew install kubectl
   install_krew_for_kubectl
   install_plugins_with_krew
+  install_kube_convert
 }
 
 [[ "$BASH_SOURCE" == "$0" ]] && main "$@"
